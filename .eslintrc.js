0d8fdcfb45cf03c96294b4948e20b28d5f67b1f1
Suraj Maheshwari <surajmaheshwari159@gmail.com>
2023-07-16 01:18:53 +0530

    fix

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+// module.exports = {
+//   // Your other ESLint configuration rules (if any) go here
+//   rules: {
+//     // Treat missing semicolons as warnings instead of errors
+//     semi: ["warn", "never"],
+//   },
+// }
